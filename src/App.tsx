66e8c2b4d12a5c8800fe0234a0e0074ec1c6327d
Battensalpozz <sebasthiansalinaspozzo@gmail.com>
2023-06-18 23:59:47 -0400

    Changes of App

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { ReactNode } from 'react';
 import './App.css';
-import Perro from "./InterfaceTs/Perro";
-import Pajaro from "./InterfaceTs/Pajaro";
-import Gato from "./InterfaceTs/Gato";
-import CustomButton from './components/CustomButton';
+import CustomButton from './common/CustomButton';
+import CustomTextBox from './common/CustomTextBox';
 
-const perro = new Perro("Firulais");
-const gato = new Gato("Garfield");
-const pajaro = new Pajaro("Tweety");
+const App = (): JSX.Element => {
+  const [textBoxValue, setTextBoxValue] = useState("");
 
-perro.hacerSonido(); 
-gato.hacerSonido();   
-pajaro.hacerSonido(); 
+  // Create a new instance of CustomButton
+  const customButton = new CustomButton({ name: "My Button" });
 
+  // Create a new instance of CustomTextBox and pass value and onChange props
+  const customTextBox = new CustomTextBox({
+    value: textBoxValue,
+    onChange: handleTextBoxChange
+  });
 
-function App() {
-  const handleClick = () => {
-    console.log('Botón clickeado');
-  };
+  // Get the ReactNode components from CustomButton and CustomTextBox
+  const customButtonComponent: ReactNode = customButton.getComponent();
+  const customTextBoxComponent: ReactNode = customTextBox.getComponent();
+
+  // Handler function for textbox value change
+  function handleTextBoxChange(value: string) {
+    setTextBoxValue(value);
+  }
 
   return (
-    <div>
-      <CustomButton text="Button" onClick={handleClick} />
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+        {customTextBoxComponent}
+        {customButtonComponent}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
